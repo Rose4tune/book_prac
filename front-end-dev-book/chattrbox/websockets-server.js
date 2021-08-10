c6e2a8adcb533c43e15c35eedc8575914c1ea6d2
Lee Ye Seo <rosefor2ne@gmail.com>
2021-08-10 20:26:14 +0900

    연결 처리, 에코기능 추가

--- front-end-dev-book/chattrbox/websockets-server.js
+++ front-end-dev-book/chattrbox/websockets-server.js
@@ -6,3 +6,14 @@ var ws = new WebSocketServer({
 });
 
 console.log('websockets server started');
+
+// 웹소켓 서버의 connection event를 위한 콜백 설정
+ws.on('connection', function (socket) {
+  console.log('client connection established');
+
+  // Echo 기능 추가
+  socket.on('message', function (data) {
+    console.log('eassage received : ' + data);
+    socket.send(data);
+  });
+});
