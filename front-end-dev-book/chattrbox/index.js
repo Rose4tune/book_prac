82beb43ffd9f62fa5d7d86bde98080b08d82d651
Lee Ye Seo <rosefor2ne@gmail.com>
2021-08-10 17:28:00 +0900

    404 오류화면 처리 함수

--- front-end-dev-book/chattrbox/index.js
+++ front-end-dev-book/chattrbox/index.js
@@ -2,12 +2,24 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract'); // extract 모듈 불러옴
 
+// errer 발생 시 처리 함수
+var handleError = function (err, res) {
+  res.writeHead(404);
+  res.end();
+}
+
 var server = http.createServer(function(req, res) {
   console.log('Responding to a request');
   // extract 모듈을 사용하여 파일 경로 처리하도록 변경함
   var filePath = extract(req.url);
   fs.readFile(filePath, function (err, data) {
-    res.end(data);
+    // 에려 발생과 아닌 상황 구분하기
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      res.end(data);
+    }
   })
 });
 
