59f6eefb86c791061660084712a59e766e026aa5
Lee Ye Seo <rosefor2ne@gmail.com>
2021-08-14 12:47:12 +0900

    기본 폴더 구조 생성, index.html 서버 연결 오류..ㅠㅠ

--- node_chat/app.js
+++ node_chat/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
+app.use('/css', express.static('./static/css'));
+app.use('/js', express.static('./static/js'));
+
 // Get 방식으로 / 경로에 접속하면 실행
 app.get('/', function(request, response) {
   console.log('유저가 / 으로 접속함');
-  response.send('Hellow, Express Server!!');
+  fs.readFile('./static/index.html', function(err, data) {
+    if(err) {
+      response.send('에러....ㅠㅠ');
+    } else {
+      response.writeHead(200, {'Content-Type':'texs/html'});
+      response.write(data);
+      response.end();
+    }
+  })
 })
 
 // 서버 8080 포트로 연결
