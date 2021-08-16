a5ee920da735009082d4aba51bd2db8aca3cdebc
Lee Ye Seo <rosefor2ne@gmail.com>
2021-08-16 15:31:02 +0900

    채팅 기능 클라이언트측, 화면에 보이기

--- node_chat/app.js
+++ node_chat/app.js
@@ -13,10 +13,10 @@ app.use('/js', express.static('./static/js'));
 
 // Get 방식으로 / 경로에 접속하면 index.html실행
 app.get('/', function(request, response) {
-  console.log('유저가 / 으로 접속함');
+  // console.log('유저가 / 으로 접속함');
   fs.readFile('./static/index.html', function(err, data) {
     if(err) {
-      response.send('에러....ㅠㅠ');
+      response.send('에러ㅠㅠ');
     } else {
       response.writeHead(200, {'Content-Type':'text/html'});
       response.write(data);
@@ -37,28 +37,28 @@ io.sockets.on('connection', function(socket) {
     // 소켓에 이름 저장
     socket.name = name;
     // 모든 소켓(io.socket.emit)에 전송
-    io.socket.emit('update', {type: 'connect', name: 'SERVER', message: name + ' 님이 접속하였습니다.'});
+    io.sockets.emit('update', {type: 'connect', message: name + ' 님이 접속하였습니다.'});
   });
 
   // (정의 이벤트) 전송한 메시지 받기
   socket.on('message', function(data) {
     // 받은 데이터 누가 보냈는지 이름 추가
-    data.name = socket.name
     console.log(data);
+    data.name = socket.name;
     // 보낸 사람 제외한 나머지 유저(socket.broadcast.emit)에게 메시지 전송
-    socket.breadcast.emit('update', data);
+    socket.broadcast.emit('update', data);
   })
 
   // (정의 이벤트)
-  socket.on('send', function(data) {
-    console.log('전달된 메시지 : ' + data.msg);
-  });
+  // socket.on('send', function(data) {
+  //   console.log('전달된 메시지 : ' + data.msg);
+  // });
 
   // (socket.io의 기본 이벤트) : 소켓과 접속이 끊어지면 자동 실행 됨.
   socket.on('disconnect', function() {
     console.log(socket.name + '님이 나가셨습니다.');
     // 나가는 사람을 제외한 나머지 유저(socket.broadcast.emit에게 메시지 전송
-    socket.breadcast.emit('update', {type: 'disconnect', name: 'SILVER', message: socket.name + '님이 나가셨습니다.'});
+    socket.broadcast.emit('update', {type: 'disconnect', message: socket.name + '님이 나가셨습니다.'});
   });
 });
 
