74d9577ba3da531a72a5ca6353ffcde74794d380
Lee Ye Seo <rosefor2ne@gmail.com>
2021-08-17 11:56:36 +0900

    Revert "Node chat practice"

--- front-end-dev-book/chattrbox/app/scripts/src/app.js
+++ front-end-dev-book/chattrbox/app/scripts/src/app.js
@@ -1,51 +1,34 @@
 import socket from './ws-client';
-import {ChatForm, ChatList} from './dom';
-
-const FORM_SELECTOR = '[data-chat="chat-form"]';
-const INPUT_SELECTOR = '[data-chat="message-input"]';
-const LIST_SELECTOR = '[data-chat="message-list"]';
 
 class ChatApp {
   constructor() {
-    this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
-    this.chatList = new ChatList(LIST_SELECTOR, 'blackWidow')
-
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(() => {
-      this.chatForm.init((data) => {
-        let message = new ChatMessage({message: data});
-        socket.sendMessage(message.serialize());
-        console.log(message);
-        console.log(message.serialize());
-      });
+      let message = new ChatMessage({ message: 'POW!'});
+      socket.sendMessage(message.serialize());
     });
     socket.registerMessageHandler((data) => {
       console.log(data);
-      let message = new ChatMessage(data);
-      this.chatList.drawMessage(message.serialize());
-      console.log(message);
-      console.log(message.serialize());
-    });
-  };
-};
+    })
+  }
+}
 
 class ChatMessage {
   constructor({
-    message: m,
-    user: u = 'Marvel',
-    timestamp: t = (new Date()).getTime()
+      message: m,
+      user: u = 'batman',
+      timestamp: t = (new Date()).getTime()
   }) {
     this.message = m;
     this.user = u;
     this.timestamp = t;
-  };
+  }
   serialize() {
     return {
       user: this.user,
       message: this.message,
       timestamp: this.timestamp
     };
-  };
-};
-
+  }
+}
 export default ChatApp;
--- front-end-dev-book/chattrbox/app/scripts/src/ws-client.js
+++ front-end-dev-book/chattrbox/app/scripts/src/ws-client.js
@@ -1,37 +1,32 @@
 let socket;
-console.log('socket : ' + socket);
 
 function init (url) {
   socket = new WebSocket(url);
   console.log('connecting...');
-};
+}
 
 function registerOpenHandler (handlerFunction) {
   socket.onopen = () => {
     console.log('open');
     handlerFunction();
   }
-};
+}
 
 function registerMessageHandler (handlerFunction) {
   socket.onmessage = (e) => {
-    console.log('message', e);
-    // let data = JSON.parse(e.data);
-    let data = e.data;
+    console.log('message', e.data);
+    let data = JSON.parse(e.data);
     handlerFunction(data);
   }
-  console.log(handlerFunction);
-};
+}
 
 function sendMessage (payload) {
-  console.log(payload);
-  // console.log(JSON.stringify(payload));
-  socket.send(payload);
-};
+  socket.send(JSON.stringify(payload));
+}
 
 export default {
   init,
   registerOpenHandler,
   registerMessageHandler,
   sendMessage
-};
+}
