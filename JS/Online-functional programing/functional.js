daf7ffff8f2251e123a80a8f6c9a1d45d7b8517c
Lee Ye Seo <rosefor2ne@gmail.com>
2022-07-22 12:10:51 +0900

    log 함수가 뭘까...?

--- JS/Online-functional programing/functional.js
+++ JS/Online-functional programing/functional.js
@@ -1,9 +1,16 @@
 const apply1 = f => f(1)
 const add2 = a => a + 2
 
-console.log(apply1(add2))
-console.log(add2(3))
+// console.log(apply1(add2))
+// console.log(add2(3))
 
 
 const f1 = () => () => 1
-console.log(f1()())
+// console.log(f1()())
+
+const times = (f, n) => {
+  let i = -1;
+  while(++i < n) f(i);
+}
+
+times(a => a + 10, 3)
