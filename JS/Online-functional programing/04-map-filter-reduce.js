22b73b8908765c14d6bf4d7376b33effc4f6f7cd
Lee Ye Seo <rosefor2ne@gmail.com>
2022-08-16 17:07:57 +0900

    패턴함수(go, pipe)

--- JS/Online-functional programing/04-map-filter-reduce.js
+++ JS/Online-functional programing/04-map-filter-reduce.js
@@ -59,7 +59,7 @@ log(new Map(map(([k, a]) => [k, a * 2], m)))
 
 
 /**
- * FILTER 함수
+ * FILTER (거르는) 함수
  */
 
 const filter = (f, iter) => {
@@ -95,10 +95,95 @@ log(filter(n => n % 2, function* () {
 
 
 
-console.clear();
+
+/**
+ * REDUCE (줄이는, 합치는) 함수
+ */
+const nums = [1, 2, 3, 4, 5];
+
+// let total = 0;
+// for (const n of nums) {
+//   total = total + n;
+// }
+
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc
+}
+
+const add = (a, b) => a + b;
+
+log(reduce(add, 0, nums))
+log(reduce(add, nums))
+
+log(reduce(
+  (total_price, products) => total_price + products.price,
+  0,
+  products));
+
+
+
+
 
 
 /**
- * REDUCE 함수
+ * MAP, FILTER, REDUCE 중첩 함수
  */
 
+log(reduce(
+  add,
+  map(p => p.price,
+    filter(p => p.price < 20000, products))));
+    
+log(reduce(
+  add,
+  filter(n => n >= 20000,
+    map(p => p.price, products))));
+
+
+
+
+
+
+
+      
+
+/**
+ * 패턴 함수 (코드를 값으로 다루어 표현력 높이기)
+ * go : 인자들을 받아서 하나의 값으로 축약을 해나간다. (함수의 순서를 뒤바꿈)
+ * pipe : 함수들이 나열되있는 합성된 함수를 만든다. (함수를 리턴하는 함수)
+ * compose
+ */
+const go = (...args) => reduce((a, f) => f(a), args);
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs) //내부에서 go를 사용하는 함수
+
+
+
+go (
+  add(0, 1),
+  // a => a + 1,
+  a => a + 10,
+  a => a + 100,
+  log);
+
+const f = pipe(
+  (a, b) => a + b,
+  a => a + 10,
+  a => a + 100);
+
+log(f(0, 1))
+
+
+go(
+  products,
+  products => filter(p => p.price < 20000, products),
+  products => map(p => p.price, products),
+  prices => reduce(add, prices),
+  log
+);
