e0b4438776462db18848b2fe0ee0e5f57abd4ddc
Lee Ye Seo <rosefor2ne@gmail.com>
2022-08-23 11:21:08 +0900

    L.map, L.filter 사용하여 map, filter 만들기

--- JS/Online-functional programing/fx.js
+++ JS/Online-functional programing/fx.js
@@ -1,26 +1,55 @@
-// CURRY FUNC
+// CURRY & GO & PIPE
 export const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+export const go = (...args) => reduce((a, f) => f(a), args);//앞선 함수의 결과 값을 뒷 함수에 인자로 전달하는 함수
+export const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 
 
-// IMMEDIATE FUNC
-export const range = l => {
-  let i = -1;
+// COMMON FUNCs
+export const add = (a, b) => a + b;
+
+
+// TAKE affiliation (take, find)
+export const take = curry((l, iter) => {
   let res = [];
-  while (++i < l) res.push(i);
-  return res;
-};
-
-export const map = curry((f, iter) => {
-  let res = [];
-  for (const a of iter) res.push(f(a));
+  for (const a of iter) {
+    res.push(a);
+    if(res.length == l) return res;
+  }
   return res;
 });
 
-export const filter = curry((f, iter) => {
-  let res = [];
-  for (const a of iter) if(f(a)) res.push(a)
-  return res
+export const takeAll = take(Infinity)
+
+export const find = curry((f, iter) => go(
+  iter,
+  L.filter(f),
+  take(3),
+  ([a]) => a
+));
+
+
+// REDUCE affiliation (reduce, join)
+export const reduce = curry((f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) acc = f(acc, a);
+  return acc
 });
+
+export const join = curry((sep = ',', iter) => reduce((a, b) => `${a}${sep}${b}`, iter));
+
+
+
+
+
+
+
+
+
+
+
 
 
 // LAZY FUNC
@@ -44,52 +73,44 @@ L.filter = curry(function* (f, iter) {
 
 
 
-
-// TAKE affiliation
-export const take = curry((l, iter) => {
+// IMMEDIATE FUNC
+export const range = l => {
+  let i = -1;
   let res = [];
-  for (const a of iter) {
-    res.push(a);
-    if(res.length == l) return res;
-  }
+  while (++i < l) res.push(i);
   return res;
-});
+};
 
-export const find = curry((f, iter) => go(
-  iter,
-  L.filter(f),
-  take(3),
-  ([a]) => a
-));
-
-// REDUCE affiliation
-export const reduce = curry((f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  }
-  for (const a of iter) acc = f(acc, a);
-  return acc
-});
-
-export const join = curry((sep = ',', iter) => reduce((a, b) => `${a}${sep}${b}`, iter));
-
-
-// COMMON FUNCs
-export const add = (a, b) => a + b;
-
-
-// GO & PIPE FUNC
-export const go = (...args) => reduce((a, f) => f(a), args);//앞선 함수의 결과 값을 뒷 함수에 인자로 전달하는 함수
-export const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
+/**
+ * ### L.map, L.filter 사용하여 map, filter 만들기###
+ *  - 인수 받는 부분 go 함수로 대체
+ *  - 첫 인수 다음 함수 내부로 정리
+ *  - curry 인수 = L.map 인수
+ *     >> (curry 인수 + go 함수) 합쳐서 pipe 함수로 변경 가능
+ *  - pipe로 변경 후 중복되는 take(Infinity) 부분 공통함수로 묶기
+ */
+// export const map = curry((f, iter) => {
+//   let res = [];
+//   for (const a of iter) res.push(f(a));
+//   return res;
+// });
+// export const map = curry((f, iter) => go(
+//   iter,
+//   L.map(f),
+//   take(Infinity)
+// ));
+// export const map = curry((f, iter) => go(
+//   L.map(f, iter),
+//   take(Infinity)
+// ));
+export const map = curry(pipe(L.map, takeAll));
+// console.log(map(a => a + 10, L.range(4)));
 
 
-
-
-
-
-
-
-
-
-
+// export const filter = curry((f, iter) => {
+//   let res = [];
+//   for (const a of iter) if(f(a)) res.push(a)
+//   return res
+// });
+export const filter = curry(pipe(L.filter, takeAll));
+// console.log(filter(a => a % 2, range(4)));
